2ea0a419890819bc3668eefc3a896af9a02c5d3c
Gentile, John <johncgentile17@gmail.com>
2019-09-20 11:21:30 -0400

    (+) initial assembly stuff
    Signed-off-by: Gentile, John <johncgentile17@gmail.com>

--- pthread/hello_world.c
+++ pthread/hello_world.c
@@ -4,25 +4,23 @@
 
 void *PrintHello(void *threadid)
 {
-        long tid;
-        tid = (long)threadid;
-        printf("Hello World! I'm in thread# %ld\n", tid);
-        pthread_exit(NULL);
+	long tid;
+	tid = (long)threadid;
+	printf("Hello World! I'm in thread# %ld\n", tid);
+	pthread_exit(NULL);
 }
 
 int main(void)
 {
-        pthread_t threads[NUM_THREADS];
-        int rc;
-        long t;
-        for(t=0; t<NUM_THREADS; t++) {
-                printf("In main: creating thread %ld\n", t);
-                rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
-                if (rc) {
-                        printf("ERRORl return code from pthread_create() is %d\n", rc);
-                        return 1;
-                }
-        }
+	pthread_t threads[NUM_THREADS];
+	for(long t=0; t<NUM_THREADS; t++) {
+		printf("In main: creating thread %ld\n", t);
+		int rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+		if (rc) {
+			printf("ERRORl return code from pthread_create() is %d\n", rc);
+			return 1;
+		}
+	}
 
-        pthread_exit(NULL);
+	pthread_exit(NULL);
 }
